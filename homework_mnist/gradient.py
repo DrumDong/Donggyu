8648d94c2aadfc50338e7f1906ff24c6dea7fa17
DrumDong <49140971+DrumDong@users.noreply.github.com>
2019-11-02 14:34:06 +0900

    Add files via upload

---
+++ homework_mnist/gradient.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+import numpy as np
+
+def numerical_gradient(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+
+        x[idx] = tmp_val  # 값 복원
+        it.iternext()
+
+    return grad
